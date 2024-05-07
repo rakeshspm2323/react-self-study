c5b8d72a2c1c3def74febdcd2d01c1e210dba1bf
Rakesh Kumar <rakeshspm2323@gmail.com>
2024-05-07 15:05:48 +0530

    second change

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
     <div className='flex justify-center items-center'>
       <p className='font-bold text-[black] text-center m-5'>My First code in React</p>
       <button className='font-bold text-[white] border-2  px-1.5 py-1.5 rounded-md border-gray-950  bg-black text-center mt-5' onClick={handleEvent} onDoubleClick={removeEvent}>Click me!</button>
+      <button className='font-bold text-[white] border-2  px-1.5 py-1.5 rounded-md border-gray-950  bg-black text-center mt-5' onClick={handleEvent} onDoubleClick={removeEvent}>Click Here! </button>
     </div>
   )
 }
