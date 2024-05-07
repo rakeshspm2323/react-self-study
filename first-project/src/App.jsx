d08ae655995c9aec18f0f33af46c69b1edfb8941
Rakesh Kumar <rakeshspm2323@gmail.com>
2024-05-07 14:59:50 +0530

    first commit

---
+++ first-project/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+const App = () => {
+  let handleEvent = () => {
+     let btn = document.querySelector('button')
+     btn.style.backgroundColor = 'deeppink';
+     btn.style.color = 'black';
+   }
+  let removeEvent =() => {
+    let btn = document.querySelector('button')
+    btn.style.backgroundColor = 'black';
+    btn.style.color = 'white';
+  }
+  return (
+    <div className='flex justify-center items-center'>
+      <p className='font-bold text-[black] text-center m-5'>My First code in React</p>
+      <button className='font-bold text-[white] border-2  px-1.5 py-1.5 rounded-md border-gray-950  bg-black text-center mt-5' onClick={handleEvent} onDoubleClick={removeEvent}>Click me!</button>
+    </div>
+  )
+}
+
+export default App
