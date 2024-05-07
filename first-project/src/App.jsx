89f0d3f3af0c8d4a6c2115548c7319b896bfed0a
Rakesh Kumar <rakeshspm2323@gmail.com>
2024-05-07 16:25:24 +0530

    props1

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,20 +1,11 @@
 import React from 'react'
+import Child from './components/props/props1/Child'
+
+
 const App = () => {
-  let handleEvent = () => {
-     let btn = document.querySelector('button')
-     btn.style.backgroundColor = 'deeppink';
-     btn.style.color = 'black';
-   }
-  let removeEvent =() => {
-    let btn = document.querySelector('button')
-    btn.style.backgroundColor = 'black';
-    btn.style.color = 'white';
-  }
   return (
-    <div className='flex justify-center items-center'>
-      <p className='font-bold text-[black] text-center m-5'>My First code in React</p>
-      <button className='font-bold text-[white] border-2  px-1.5 py-1.5 rounded-md border-gray-950  bg-black text-center mt-5' onClick={handleEvent} onDoubleClick={removeEvent}>Click me!</button>
-      <button className='font-bold text-[white] border-2  px-1.5 py-1.5 rounded-md border-gray-950  bg-black text-center mt-5' onClick={handleEvent} onDoubleClick={removeEvent}>Click Here! </button>
+    <div>
+      <Child userName='Rakesh' city='Noida' ></Child>
     </div>
   )
 }
