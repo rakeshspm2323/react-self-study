e558550f1870c565cbb37df00f6137f9857ecc47
Rakesh Kumar <rakeshspm2323@gmail.com>
2024-05-10 12:53:19 +0530

    Card Props

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,12 +1,52 @@
 import React from 'react'
 import './App.css'
-import Card from './components/props/props2/Card';
+import hrithik from './assets/HRX.jpeg'
+import prabhas from './assets/Prabhas.jpg'
+import rk from './assets/R kapoor.avif'
+import Card from './components/props/props3/Card'
+
+
 
 
 const App = () => {
+
+  let actor1={
+    actorName:'Hrithik Roshan',
+    about: `Hrithik Roshan is an Indian actor who works in Hindi cinema. He has portrayed a variety of characters and is known for his dancing skills.
+     One of the highest-paid actors in India.`,
+    movie1:'War',
+    movie2:'Krrish',
+    movie3:'Lakshya',
+    image: hrithik
+  }
+
+  let actor2={
+    actorName:'Prabhas',
+    about: 'Uppalapati Venkata Suryanarayana Prabhas Raju, known mononymously as Prabhas, is an Indian actor who works predominantly in Telugu cinema.',
+    movie1:'Bahubali',
+    movie2:'Sallar',
+    movie3:'Rebel',
+    image: prabhas
+  }
+
+  let actor3={
+    actorName:'',
+    about: `Ranbir Kapoor is an Indian actor known for his work in Hindi-language films. 
+    He is one of the highest-paid actors of Hindi cinema and has featured in Forbes India's Celebrity 100 list since 2012.`,
+    movie1:'Animal',
+    movie2:'Shamshera',
+    movie3:'Brahmastra',
+    image: rk
+  }
+
+
+
   return (
-    <div>
-      <Card></Card>
+    <div className='flex items-center justify-center gap-[2rem] pt-12'>
+      <Card user={actor1}></Card>
+      <Card user={actor2}></Card>
+      <Card user={actor3}></Card>
+      
     </div>
   )
 }
---
+++ first-project/src/components/props/props3/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Card = (props) => {
+  return (
+        <div className='w-[350px] h-[450px] bg-slate-100 rounded-md text-center pb-5 '>
+        <img src={props.user.image} className='w-[100%] h-[200px] rounded-md'/>
+        <h2 className='text-[1.65rem] font-semibold my-2'>{props.user.actorName}</h2>
+        <p className='text-justify px-5 mb-4 text-[1.15rem]'>{props.user.about}</p>
+        <div className='flex gap-4 justify-center'>
+            <p className='font-semibolde bg-[#1581ab] text-[white] py-1 px-2 text-[0.7rem] rounded w-[fit-content] '>{props.user.movie1}</p>
+            <p className='font-semibolde bg-[#1581ab] text-[white] py-1 px-2 text-[0.7rem] rounded w-[fit-content]'>{props.user.movie2}</p>
+            <p className='font-semibolde bg-[#1581ab] text-[white] py-1 px-2 text-[0.7rem] rounded w-[fit-content]'>{props.user.movie3}</p>
+        </div>
+        </div>
+  )
+}
+
+export default Card
